4d221d5e2cd3e97a5420d6061360fd9fe5fec506
ttawan.mohr <tannarak2009@gmail.com>
2022-03-23 10:36:06 +0800

    create edit.blade, create button edit

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -47,12 +47,18 @@
                                         <td>{{ $todo->name }}</td>
                                         <td>{{ $todo->status }}</td>
                                         <td>
-                                            <button class="btn btn-primary">Done</button>
-                                            <form action="todos/{{ $todo->id }}" method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">Delete</button>
-                                            </form>
+                                            <div class="d-flex">
+                                                <a href="/todos/{{ $todo->id }}/edit"
+                                                    class="btn btn-warning mx-2">Edit</a>
+
+                                                <button class="btn btn-primary mx-2">Done</button>
+
+                                                <form action="todos/{{ $todo->id }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger mx-2">Delete</button>
+                                                </form>
+                                            </div>
                                         </td>
                                     </tr>
                                 @empty
